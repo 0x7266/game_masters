2d078fe9278b756de1d621c220dcadca21ba7e38
unknown <rrrr0ffff@gmail.com>
2023-06-21 19:42:32 -0300

    basic app.tsx layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Header from "./components/Header";
+import GameList from "./components/GameList";
+
 function App() {
 	return (
-		<>
-			<div>Hello, World!</div>
-		</>
+		<div className="min-h-screen p-10 bg-gradient-to-r from-am-blue-darker from-[-22.45%] to-am-blue-lighter to-[105.12%] flex flex-col items-center">
+			<div className="container flex flex-col items-center gap-20 text-white">
+				<Header />
+				<GameList />
+			</div>
+		</div>
 	);
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+	return (
+		<div className="flex flex-col justify-center items-center gap-10">
+			<div className="logo flex gap-5 items-center">
+				<div className="text-6xl">🕹</div>
+				<h1 className="text-6xl">Game List</h1>
+			</div>
+			<input type="text" />
+		</div>
+	);
+}
