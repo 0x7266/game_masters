95e7c37eca711c9a4f77181dbb3007a8225c65e8
unknown <rrrr0ffff@gmail.com>
2023-06-22 11:45:45 -0300

    header events

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,51 @@
+import { useContext } from "react";
+import { GamesContext } from "../contexts/GamesContext";
+import { useFilter } from "../hooks/useFilter";
+
 export default function Header() {
+	const genders: string[] = [
+		"All",
+		"Action RPG",
+		"ARPG",
+		"Battle Royale",
+		"Card Game",
+		"Fantasy",
+		"Fighting",
+		"MMO",
+		"MMOARPG",
+		"MMORPG",
+		"MOBA",
+		"Racing",
+		"Social",
+		"Shooter",
+		"Sports",
+		"Strategy",
+	];
+
+	const { filter } = useFilter();
 	return (
-		<div className="flex flex-col justify-center items-center gap-10">
+		<div className="max-w-3xl flex flex-col justify-center items-center gap-10">
 			<div className="logo flex gap-5 items-center">
 				<div className="text-6xl">🕹</div>
 				<h1 className="text-6xl">Game List</h1>
 			</div>
-			<input type="text" />
+			<input
+				className="w-full rounded px-2 py-3 text-black font-semibold"
+				type="text"
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => filter(e)}
+			/>
+			<div className="w-2/3 flex flex-wrap justify-center gap-2">
+				{genders.map((gender) => (
+					<button
+						className="rounded border px-3 py-1"
+						key={gender}
+						value={gender}
+						onClick={(e) => filter(e)}
+					>
+						{gender}
+					</button>
+				))}
+			</div>
 		</div>
 	);
 }
