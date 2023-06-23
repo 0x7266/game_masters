2942d07ff0bd22ade2836fb8348792c4090e2338
unknown <rrrr0ffff@gmail.com>
2023-06-23 01:41:31 -0300

    fix: user can see the loader again

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -7,36 +7,33 @@ import Error from "./Error";
 
 export default function GameList() {
 	const { filteredGames, isLoading } = useContext(GamesContext);
-
-	if (filteredGames.status >= 200 && filteredGames.status <= 209) {
-		return (
-			<motion.div
-				animate={{ opacity: 1 }}
-				initial={{ opacity: 0 }}
-				exit={{ opacity: 0 }}
-			>
-				{isLoading ? (
-					<Loader />
-				) : (
-					<motion.div layout className="grid md:grid-cols-3 gap-3 p-2">
-						<AnimatePresence>
-							{filteredGames.games.map((game) => (
-								<GameCard {...game} key={game.id} />
-							))}
-						</AnimatePresence>
-					</motion.div>
-				)}
-			</motion.div>
-		);
-	}
-
 	if (filteredGames.status >= 500 && filteredGames.status <= 509) {
 		return (
 			<Error message="O servidor falhou em responder, tente recarregar a página." />
 		);
 	}
-
+	if (filteredGames.status >= 300) {
+		return (
+			<Error message="O servidor não conseguirá responder por agora, tente voltar novamente mais tarde" />
+		);
+	}
 	return (
-		<Error message="O servidor não conseguirá responder por agora, tente voltar novamente mais tarde" />
+		<motion.div
+			animate={{ opacity: 1 }}
+			initial={{ opacity: 0 }}
+			exit={{ opacity: 0 }}
+		>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<motion.div layout className="grid md:grid-cols-3 gap-3 p-2">
+					<AnimatePresence>
+						{filteredGames.games.map((game) => (
+							<GameCard {...game} key={game.id} />
+						))}
+					</AnimatePresence>
+				</motion.div>
+			)}
+		</motion.div>
 	);
 }
